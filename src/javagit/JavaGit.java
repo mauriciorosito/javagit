5b29820f9ef5d9209015b80b7817be01d5bf3af1
mauricio.rosito <mauricio.rosito@192.168.3.81>
2014-11-04 13:04:55 -0200

    commit

---
+++ src/javagit/JavaGit.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagit;
+
+/**
+ *
+ * @author mauricio.rosito
+ */
+public class JavaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 1;
+    }
+}
